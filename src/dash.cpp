23b19b9627923f3631a141a9febab0d5fc0da5b6
MadduxLofton <Maddux.lofton@gmail.com>
2023-04-13 17:13:57 -0500

    Build Errors fixed

--- src/dash.cpp
+++ src/dash.cpp
@@ -33,14 +33,14 @@ uint16_t yellowR;
 uint16_t yellowG;
 uint16_t yellowB;
 
-uint16_t rpm;
-uint16_t bat;
-uint16_t afrtgt;
-uint16_t clt;
-uint16_t afr;
-uint16_t gear;
-uint16_t spd;
-uint16_t sync;
+uint16_t rpm_dash;
+uint16_t bat_dash;
+uint16_t afrtgt_dash;
+uint16_t clt_dash;
+uint16_t afr_dash;
+uint16_t gear_dash;
+uint16_t spd_dash;
+uint16_t sync_dash;
 
 static CAN_message_t txmsg, rxmsg;
 
@@ -75,41 +75,42 @@ const byte DIR = 18; //A4 or SEN2 in schematic
 
 // need to take in rpm and represent it as 800/10500, then round down in pixel calculations
 // every time a can message is received, sniff it and make sure it's something we want
+
 void canSniff(const CAN_message_t &msg) {
 
   // switch statement better than nested ifs
   switch(msg.id) { // ID's 1520+ are Megasquirt CAN broadcast frames
 
     case 1520: // group 0
-      rpm = (int)(word(msg.buf[6], rxmsg.buf[7]));
+      rpm_dash = (int)(word(msg.buf[6], rxmsg.buf[7]));
     break;
     
     case 1521: // group 1
-      afrtgt = (int)(word(0x00, msg.buf[4]));
+      afrtgt_dash = (int)(word(0x00, msg.buf[4]));
     break;
 
     case 1522: // group 2
-      clt = (int)(word(msg.buf[6], msg.buf[7]));
+      clt_dash = (int)(word(msg.buf[6], msg.buf[7]));
     break;
 
     case 1523: // group 3
-      bat = (int)(word(msg.buf[2], msg.buf[3]));
+      bat_dash = (int)(word(msg.buf[2], msg.buf[3]));
     break;
 
     case 1551: // group 31
-      afr = (int)(word(0x00, msg.buf[0]));
+      afr_dash = (int)(word(0x00, msg.buf[0]));
     break;
 
     case 1553: // group 33
-      gear = (int)(word(0x00, msg.buf[6]));
+      gear_dash = (int)(word(0x00, msg.buf[6]));
     break;
 
     case 1562: // group 42
-      spd = (int)(word(msg.buf[0], msg.buf[1]));
+      spd_dash = (int)(word(msg.buf[0], msg.buf[1]));
     break;
 
     case 1563: // group 43
-      sync = (int)(word(0x00, msg.buf[0]));
+      sync_dash = (int)(word(0x00, msg.buf[0]));
     break;
 
     default: // in the case of not a broadcast packet
@@ -143,14 +144,14 @@ void Initialize_Dash(void)
     yellowG = 252;
     yellowB = 13;
 
-    rpm = 0;
-    bat = 0;
-    afrtgt = 0;
-    clt = 0;
-    afr = 0;
-    gear = 0;
-    spd = 0;
-    sync = 0;
+    rpm_dash = 0;
+    bat_dash = 0;
+    afrtgt_dash = 0;
+    clt_dash = 0;
+    afr_dash = 0;
+    gear_dash = 0;
+    spd_dash = 0;
+    sync_dash = 0;
 
     
 
@@ -240,7 +241,7 @@ void updateData(void)
         b3x2 = b2Bound;
     }
 
-    rpm = barRpm * 10500 / 800;
+    rpm_dash = barRpm * 10500 / 800;
 
     if (testing)
     {
@@ -274,7 +275,7 @@ void updateData(void)
     }
 }
 
-uint16_t Add_Diag_To_Display_List(uint16_t FWol){
+uint16_t Add_Diag_To_Display_List_dash(uint16_t FWol){
     
     FWol = EVE_Cmd_Dat_0(FWol, EVE_ENC_COLOR_RGB(greenR, greenG, greenB));
     
@@ -287,23 +288,144 @@ uint16_t Add_Diag_To_Display_List(uint16_t FWol){
     FWol = EVE_Cmd_Dat_0(FWol, EVE_ENC_COLOR_RGB(greenR, greenG, greenB));
 
     // switched to bat temporarily
-    FWol = EVE_PrintF(FWol, 250, 130, 25, EVE_OPT_CENTER, "RPM:  %3d", bat);
+    FWol = EVE_PrintF(FWol, 250, 130, 25, EVE_OPT_CENTER, "RPM:  %3d", bat_dash);
 
-    FWol = EVE_PrintF(FWol, 195, 205, 25, EVE_OPT_CENTER, "BATT VOLT:  %3d", bat);
+    FWol = EVE_PrintF(FWol, 195, 205, 25, EVE_OPT_CENTER, "BATT VOLT:  %3d", bat_dash);
 
-    FWol = EVE_PrintF(FWol, 211, 280, 25, EVE_OPT_CENTER, "TGT AFR:  %3d", afrtgt);
+    FWol = EVE_PrintF(FWol, 211, 280, 25, EVE_OPT_CENTER, "TGT AFR:  %3d", afrtgt_dash);
 
-    FWol = EVE_PrintF(FWol, 247, 355, 25, EVE_OPT_CENTER, "AFR:  %3d", afr);
+    FWol = EVE_PrintF(FWol, 247, 355, 25, EVE_OPT_CENTER, "AFR:  %3d", afr_dash);
 
-    FWol = EVE_PrintF(FWol, 567, 130, 25, EVE_OPT_CENTER, "CLT:  %3d", clt);
+    FWol = EVE_PrintF(FWol, 567, 130, 25, EVE_OPT_CENTER, "CLT:  %3d", clt_dash);
 
-    FWol = EVE_PrintF(FWol, 553, 205, 25, EVE_OPT_CENTER, "GEAR:  %3d", gear);
+    FWol = EVE_PrintF(FWol, 553, 205, 25, EVE_OPT_CENTER, "GEAR:  %3d", gear_dash);
 
-    FWol = EVE_PrintF(FWol, 542, 280, 25, EVE_OPT_CENTER, "SPEED:  %3d", spd);
+    FWol = EVE_PrintF(FWol, 542, 280, 25, EVE_OPT_CENTER, "SPEED:  %3d", spd_dash);
 
-    FWol = EVE_PrintF(FWol, 550, 355, 25, EVE_OPT_CENTER, "SYNC:  %3d", sync);
+    FWol = EVE_PrintF(FWol, 550, 355, 25, EVE_OPT_CENTER, "SYNC:  %3d", sync_dash);
     
     return FWol;
+}
+
+void shiftUp() {
+  oldGear = gear_dash;
+  
+  //digitalWrite(C_SPAR, 1); // cut spark
+  digitalWrite(DIR, 1); // set direction
+  digitalWrite(PUL, 1); // shift motor
+  delay(upShiftTime);   // keep shifting
+  digitalWrite(PUL, 0); // stop shifting
+  //digitalWrite(C_SPAR, 0); // resume spark
+  
+  if(oldGear == gear_dash){ // if the shift didn't occur, aka gear before = gear after
+    if(oldGear != 6){
+      if(shiftTry < REJTRY){
+        shiftTry = shiftTry + 1; //keep a count of how many tries it took
+        shiftUp();
+      } else {
+        rejectFlag = true; // if it took REJTRY tries, send a reject signal
+      }
+    } else {
+      rejectFlag = true; //if the gear is 6, GPOS can not be accurately determined
+    }
+  }
+}
+
+void shiftDown() {
+  oldGear = gear_dash;
+  if(rpm_dash < U_L_RPM) { //if RPM too high, don't even attempt to shift
+    
+    digitalWrite(DIR, 0);
+    digitalWrite(PUL, 1);
+    delay(dnShiftTime);
+    digitalWrite(PUL, 0);
+    
+    if(oldGear == gear_dash){ // if the shift didn't occur, aka gear before = gear after
+      if(oldGear != 6){ 
+        if(shiftTry < REJTRY){
+          shiftTry = shiftTry + 1; //keep a count of how many tries it took
+          shiftDown(); //try again
+        } else {
+          rejectFlag = true; // if it took REJTRY tries, send a reject signal
+        }
+      } else {
+        rejectFlag = true; //if the gear is 6, GPOS can not be accurately determined
+      }
+    }
+  } else {
+    rejectFlag = true;
+  }
+}
+
+void shiftNeutral() {
+   // car can go to neutral if in 2nd or 1st gear
+  while(gear_dash > 2){
+    shiftDown();
+  }
+  oldGear = gear_dash;
+  
+  if (oldGear == 2){
+    // neutral requires a half shift
+
+    digitalWrite(DIR, 0); // shift down
+    digitalWrite(PUL, 1);
+    delay(nuShiftTime);
+    digitalWrite(PUL, 0);
+    
+  } else if(oldGear == 1) {
+    // neutral requires a half shift
+
+    digitalWrite(DIR, 1); // shift up
+    digitalWrite(PUL, 1);
+    delay(nuShiftTime);
+    digitalWrite(PUL, 0);
+  }
+
+  if (gear_dash == oldGear){ //if the shift didn't work, try again
+    if(oldGear != 6){
+      if(shiftTry < REJTRY){
+        shiftNeutral(); 
+        shiftTry = shiftTry + 1; //keep a count of how many tries it took
+      } else {
+        rejectFlag = true; //if it took REJTRY tries, send a reject signal
+      }
+    } else {
+      rejectFlag = true;
+    }
+  }
+}
+
+
+void loop_dash() {
+
+  // updates the states of the pins
+  upPaddle.update();
+  dnPaddle.update();
+
+  //makes it so the rest of the code is executed only on a change in button state
+  if (upPaddle.changed() || dnPaddle.changed()){
+      
+    // actual shifting up and down
+    if(upPaddle.fell() && dnPaddle.fell()){
+      shiftNeutral();
+      shiftTry = 1;
+    } else if(upPaddle.fell() && !dnPaddle.changed()){
+      shiftUp();
+      shiftTry = 1;
+    } else if(!upPaddle.changed() && dnPaddle.fell()){
+      shiftDown();
+      shiftTry = 1;
+    }
+  
+    // sends a signal to dash for rejected shift
+    if(rejectFlag == true){
+      //digitalWrite(FAIL, 1);
+      delay(10);
+      rejectFlag = false;
+      //digitalWrite(FAIL, 0);
+    }
+  }
+
 }
 
 uint16_t Add_Dash_To_Display_List(uint16_t FWol, bool dashMode)
@@ -326,7 +448,7 @@ uint16_t Add_Dash_To_Display_List(uint16_t FWol, bool dashMode)
 
         FWol = EVE_PrintF(FWol, 200, 200, 25, EVE_OPT_CENTER, "RPM: ");
 
-        FWol = EVE_PrintF(FWol, 300, 200, 25, EVE_OPT_CENTER, "%3d", rpm);
+        FWol = EVE_PrintF(FWol, 300, 200, 25, EVE_OPT_CENTER, "%3d", rpm_dash);
 
         // This is where the Gear functions get printed out *****************
 
@@ -358,13 +480,13 @@ uint16_t Add_Dash_To_Display_List(uint16_t FWol, bool dashMode)
     }
     else{
         
-        FWol = Add_Diag_To_Display_List(FWol);
+        FWol = Add_Diag_To_Display_List_dash(FWol);
         
 
     }
 
     return (FWol);
-    loop();
+    loop_dash();
 }   
 
 
@@ -482,125 +604,7 @@ uint16_t gear5(uint16_t FWolE)
     return FWolE;
 }
 
-void shiftUp() {
-  oldGear = gear;
-  
-  //digitalWrite(C_SPAR, 1); // cut spark
-  digitalWrite(DIR, 1); // set direction
-  digitalWrite(PUL, 1); // shift motor
-  delay(upShiftTime);   // keep shifting
-  digitalWrite(PUL, 0); // stop shifting
-  //digitalWrite(C_SPAR, 0); // resume spark
-  
-  if(oldGear == gear){ // if the shift didn't occur, aka gear before = gear after
-    if(oldGear != 6){
-      if(shiftTry < REJTRY){
-        shiftTry = shiftTry + 1; //keep a count of how many tries it took
-        shiftUp();
-      } else {
-        rejectFlag = true; // if it took REJTRY tries, send a reject signal
-      }
-    } else {
-      rejectFlag = true; //if the gear is 6, GPOS can not be accurately determined
-    }
-  }
-}
 
-void shiftDown() {
-  oldGear = gear;
-  if(rpm < U_L_RPM) { //if RPM too high, don't even attempt to shift
-    
-    digitalWrite(DIR, 0);
-    digitalWrite(PUL, 1);
-    delay(dnShiftTime);
-    digitalWrite(PUL, 0);
-    
-    if(oldGear == gear){ // if the shift didn't occur, aka gear before = gear after
-      if(oldGear != 6){ 
-        if(shiftTry < REJTRY){
-          shiftTry = shiftTry + 1; //keep a count of how many tries it took
-          shiftDown(); //try again
-        } else {
-          rejectFlag = true; // if it took REJTRY tries, send a reject signal
-        }
-      } else {
-        rejectFlag = true; //if the gear is 6, GPOS can not be accurately determined
-      }
-    }
-  } else {
-    rejectFlag = true;
-  }
-}
-
-void shiftNeutral() {
-   // car can go to neutral if in 2nd or 1st gear
-  while(gear > 2){
-    shiftDown();
-  }
-  oldGear = gear;
-  
-  if (oldGear == 2){
-    // neutral requires a half shift
-
-    digitalWrite(DIR, 0); // shift down
-    digitalWrite(PUL, 1);
-    delay(nuShiftTime);
-    digitalWrite(PUL, 0);
-    
-  } else if(oldGear == 1) {
-    // neutral requires a half shift
-
-    digitalWrite(DIR, 1); // shift up
-    digitalWrite(PUL, 1);
-    delay(nuShiftTime);
-    digitalWrite(PUL, 0);
-  }
-
-  if (gear == oldGear){ //if the shift didn't work, try again
-    if(oldGear != 6){
-      if(shiftTry < REJTRY){
-        shiftNeutral(); 
-        shiftTry = shiftTry + 1; //keep a count of how many tries it took
-      } else {
-        rejectFlag = true; //if it took REJTRY tries, send a reject signal
-      }
-    } else {
-      rejectFlag = true;
-    }
-  }
-}
-
-void loop() {
-
-  // updates the states of the pins
-  upPaddle.update();
-  dnPaddle.update();
-
-  //makes it so the rest of the code is executed only on a change in button state
-  if (upPaddle.changed() || dnPaddle.changed()){
-      
-    // actual shifting up and down
-    if(upPaddle.fell() && dnPaddle.fell()){
-      shiftNeutral();
-      shiftTry = 1;
-    } else if(upPaddle.fell() && !dnPaddle.changed()){
-      shiftUp();
-      shiftTry = 1;
-    } else if(!upPaddle.changed() && dnPaddle.fell()){
-      shiftDown();
-      shiftTry = 1;
-    }
-  
-    // sends a signal to dash for rejected shift
-    if(rejectFlag == true){
-      //digitalWrite(FAIL, 1);
-      delay(10);
-      rejectFlag = false;
-      //digitalWrite(FAIL, 0);
-    }
-  }
-
-}
 
 /*
 
